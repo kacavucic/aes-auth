f2e6d9715bfbe68d2eb4beda6ebc3148f41340bb
Katarina Vucic <vucic.kat@gmail.com>
2022-08-26 13:51:59 +0200

    Initial commit

---
+++ src/main/java/com/zrs/aesauth/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,7 @@
+package com.zrs.aesauth.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+}
---
+++ src/test/java/com/zrs/aesauth/AesAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrs.aesauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AesAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
